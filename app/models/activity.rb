bf2735f8e05ea6130c95dc21fe32e4ed7a7c3889
Lucas Laurencich <lucas.maximiliano89@gmail.com>
2021-06-01 12:18:02 +0200

    associations done

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,11 @@
 class Activity < ApplicationRecord
   belongs_to :user
+  has many :activity_categories
+  has_many :categories, through: :activity_categories
+  has_many :favorites
+  has_many :users, through: :favorites
+  has_many :bookings
+  has_many :users, through: :bookings
+  has_many :reviews
+  has_many :users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :favorites
+  has_many :activities, through: :favorites
+  has_many :bookings
+  has_many :activities, through: :bookings
+  has_many :reviews
+  has_many :activities, through: :reviews
+  has_many :provided_activities, class_name: 'Activity', foreign_key: :user_id
 end
