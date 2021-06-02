193b00984e48cfc9f37f561ce81762dcdeaebc38
Daniel Watts <daniel.h.watts@gmail.com>
2021-06-02 16:23:48 +0200

    added mapbox

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @activities = Activity.all
+
+    @markers = @activities.geocoded.map do |activity|
+      {
+        lat: activity.latitude,
+        lng: activity.longitude
+      }
+    end
   end
 end
