557d88964f7783402c872e1f90d666d78e8bcb54
Daniel Watts <daniel.h.watts@gmail.com>
2021-06-02 11:52:43 +0200

    commit structural UI updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# db/seeds.rb
+#puts "Cleaning database..."
+#Activity.destroy_all
+
+#puts "Creating museums..."
+#dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
+#pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+
+#[ dishoom, pizza_east ].each do |attributes|
+  #activity = Activity.create!(attributes)
+  #puts "Created #{activity.name}"
+#end
+#puts "Finished!"
