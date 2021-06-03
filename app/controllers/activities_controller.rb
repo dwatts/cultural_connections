e4a042d08612d5e81ffbab98794c37a75e44e14d
Lucas Laurencich <lucas.maximiliano89@gmail.com>
2021-06-03 11:56:25 +0200

    search function + minor fix on index card

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,14 @@ class ActivitiesController < ApplicationController
 
   def index
     @activities = policy_scope(Activity)
+    if params[:query].present?
+      sql_query = " \
+        activities.name @@ :query \
+        OR activities.description @@ :query \
+        OR activities.address @@ :query \
+      "
+      @activities = Activity.where(sql_query, query: "%#{params[:query]}%")
+    end
   end
 
   def show
