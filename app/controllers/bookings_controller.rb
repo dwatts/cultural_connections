855de19178b00c6a660c402ad07780ab85265a85
Francesca Fenu <fenufrancesca@yahoo.de>
2021-06-08 13:06:04 +0200

    basic reservation without map

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,13 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update]
+  
   def new
     @booking = Booking.new
     @activity = Activity.find(params[:activity_id])
     authorize @booking
+  end
+
+  def show
   end
 
   def create
@@ -13,25 +18,26 @@ class BookingsController < ApplicationController
     @booking.status = true
     authorize @booking
     if @booking.save
-      redirect_to activity_path(@booking.activity)
+      redirect_to booking_path(@booking), notice: "Reservation created!"
     else
       render :new
     end
   end
 
   def edit
-    @booking = Booking.find(params[:id])
-    authorize @booking
   end
 
   def update
-    @booking = Booking.find(params[:id])
-    authorize @booking
     @booking.update(booking_params)
-    redirect_to dashboard_path
+    redirect_to dashboard_path, notice: "Reservation cancelled!"
   end
 
   private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
 
   def booking_params
     params.require(:booking).permit(:date, :number_of_people, :status)
