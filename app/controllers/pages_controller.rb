f6593e3598035eb796d1503c8d3bd9e080e60938
XueyingHou <houxueyingmei@gmail.com>
2021-06-09 15:46:23 +0200

    about_us page and some front fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about_us ]
 
   def home
     @activities = Activity.all
